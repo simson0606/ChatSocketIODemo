fbac4c86734e95ecba00462eb35987069b9c03ae
Simon Heij <sheij1@avans.nl>
2019-02-19 14:19:17 +0100

    Initial Commit

---
+++ ChatSocketIODemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatSocketIODemo
+//
+//  Created by simon heij on 19-02-19.
+//  Copyright © 2019 simon heij. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
